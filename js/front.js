55bbf7b149bc687ced4b7a31509e195bfe55e0a8
nikolay <artelbear@gmail.com>
2018-04-20 13:03:34 +0300

    good look

--- js/front.js
+++ js/front.js
@@ -17,13 +17,13 @@ let RASPISANIE = {
     "5": `2. Платформы
 3. БЖД
 4. БЖД`,
-    "6": `Выходной. Спи спокойно`,
-    "7": `Выходной. Спи спокойно`,
-    "0": `Выходной. Спи спокойно`,
+    "6": `Пар нет`,
+    "7": `Пар нет`,
+    "0": `Пар нет`,
 };
 let DAYS = {
     "0": "Воскресенье",
-    "1": "Понидельник к 9:00",
+    "1": "Понедельник к 9:00",
     "2": "Вторник к 10:30",
     "3": "Среда к 10:30",
     "4": "Четверг к 12:20",
@@ -67,6 +67,15 @@ var today = new Vue({
         alerted: alertmessage != "mute"
     }
 })
+
+var zav = new Vue({
+    el: '#zav',
+    data: {
+        pair: DAYS[daynow+1],
+        rasp: RASPISANIE[daynow+1],
+    }
+})
+
 var alarm = new Vue({
     el: '#alarm',
     data: {
@@ -74,7 +83,7 @@ var alarm = new Vue({
     },
     methods: {
         greet: function (event) {
-            let u = prompt('U... Wathca saayyy:') || "Есть изменения в расписании. Уточните у старосты.";
+            let u = prompt('Что!?') || "Есть изменения в расписании. Уточните у старосты.";
             if (u == "mute") {
                 today.alerted = false;
                 db.collection("important").doc("alert").delete();
@@ -88,3 +97,26 @@ var alarm = new Vue({
         }
     }
 })
+
+var raspis = new Vue({
+    el: '#raspis',
+    data: {
+        items: [
+            {day: DAYS[1] + "\n" + RASPISANIE[1]},
+            {day: DAYS[2] + "\n" + RASPISANIE[2]},
+            {day: DAYS[3] + "\n" + RASPISANIE[3]},
+            {day: DAYS[4] + "\n" + RASPISANIE[4]},
+            {day: DAYS[5] + "\n" + RASPISANIE[5]},
+        ]
+    }
+})
+
+let xut = 0
+
+function reblock() {
+    xut += 1
+    if (xut > 6) {
+        visitsinformation.seen = !visitsinformation.seen;
+        alarm.seen = !alarm.seen;
+    }
+}
